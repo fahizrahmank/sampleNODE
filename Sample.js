8f858c290ae0d75975be862b71beae4acd9c3c1f
fahizrahmank <fahizrahmank@gmail.com>
2023-08-16 13:31:55 +0530

    update

--- Sample.js
+++ Sample.js
@@ -16,4 +16,51 @@ app.patch("/", (req, res) => {
   res.send("this is new ................");
 });
 
-app.listen(8000, () => console.log("server started"));
+app.listen(8000, () => console.log("server started"));       
+
+
+
+
+
+
+
+const firstPromise = new Promise((res, rej) => {
+  setTimeout(res, 1000, 'one 1');
+});
+
+const secondPromise = new Promise((res, rej) => {
+  setTimeout(res, 100, 'two 2');
+});
+
+
+
+
+const solve=async()=>{
+  try{
+    const result1=await firstPromise
+    const result2=await secondPromise
+
+    console.log(result1)
+    console.log(result2)
+  }catch(err){
+    console.log("errpr",err)
+  }
+}
+fetch('https://fakestoreapi.com/products')
+.then((res) => res.json())
+.then(json=> console.log(json))
+solve()
+
+
+const solve2=async()=>{
+  try{
+    const result1= await fetch('https://fakestoreapi.com/products')
+    const result2=await result1.json()
+
+    // console.log(result1)
+    console.log(result2)
+  }catch(err){
+    console.log("errpr",err)
+  }
+}
+solve2()
